99915714ac07f8fb63204346c7dafce51407b823
Sully <sullybjimenez@gmail.com>
2022-09-20 22:52:12 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let myAge = 27; //Sets the human age
+let earlyYears = 2; //Sets variable to 2
+earlyYears = earlyYears*10.5;
+let laterYears = myAge-2; // sets the variable for later years
+laterYears = laterYears*4; // muliplies the variable by 4
+
+let myAgeInDogYears = earlyYears + laterYears; // sets the variable by adding these two together
+
+let myName = "Sully Jimenez".toLowerCase(); // sets the variable equal to my name and uses the toLowerCase method to convert to all lowercase 
+
+//console.log(earlyYears + " " + laterYears + " " + myAgeInDogYears);
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`); // This line puts the variables together in a string sentence
